53440d41ad9c4d2b844e677bdf4a8d780d616409
til.baniya@gmail.com <Bhutan123**>
2020-06-18 19:00:14 -0400

    Awesome Dialog is working perfectly

--- lib/screens/answerSelectionPage.dart
+++ lib/screens/answerSelectionPage.dart
@@ -1,6 +1,6 @@
 import 'dart:math';
 
-import 'package:animated_button/animated_button.dart';
+import 'package:animated_button/animated_button.dart'; //it is imported from awesome_dialog
 import 'package:flutter/material.dart';
 import 'package:circular_countdown/circular_countdown.dart';
 import 'package:assets_audio_player/assets_audio_player.dart';
@@ -10,9 +10,9 @@ import 'package:voicelytrivia/model/dataProvider.dart';
 import 'package:voicelytrivia/model/question.dart';
 import 'package:voicelytrivia/model/subCategory.dart';
 
+import 'package:awesome_dialog/awesome_dialog.dart' hide AnimatedButton;
 
 class AnswerSelectionPage extends StatefulWidget {
-
   AnswerSelectionPage(this.questionsList);
   final SubCategory questionsList;
   @override
@@ -20,19 +20,22 @@ class AnswerSelectionPage extends StatefulWidget {
 }
 
 class _AnswerSelectionPageState extends State<AnswerSelectionPage> {
-
   Question _question;
   List<String> _multipleChoices;
   String _correctAnswer;
 
-  void getMultipleChoice(){
-     _question = widget.questionsList.shuffle()[0];
+  void getMultipleChoice() {
+    _question = widget.questionsList.shuffle()[0];
     Random random = Random();
 
-    _multipleChoices = [_question.correctAnswer,_question.wrongAnswer1,_question.wrongAnswer2,_question.wrongAnswer3];
+    _multipleChoices = [
+      _question.correctAnswer,
+      _question.wrongAnswer1,
+      _question.wrongAnswer2,
+      _question.wrongAnswer3
+    ];
     _correctAnswer = _multipleChoices[0];
     _multipleChoices.shuffle(random);
-
   }
 
   //PageController _pageController = PageController(initialPage: 0);
@@ -40,7 +43,7 @@ class _AnswerSelectionPageState extends State<AnswerSelectionPage> {
   final assetsAudioPlayer = AssetsAudioPlayer();
   int centeredTextTime;
 
-  void play()async{
+  void play() async {
     try {
       await assetsAudioPlayer.open(
         Audio.network(_question.questionUrl),
@@ -49,48 +52,114 @@ class _AnswerSelectionPageState extends State<AnswerSelectionPage> {
       //mp3 unreachable
     }
   }
+
   @override
   void initState() {
     // TODO: implement initState
     super.initState();
     getMultipleChoice();
     play();
+  }
 
-
+  void stopAudio(){
+    assetsAudioPlayer.stop();
+    assetsAudioPlayer.dispose();
   }
 
   @override
   void dispose() {
     // TODO: implement dispose
     super.dispose();
-   assetsAudioPlayer.stop();
-   assetsAudioPlayer.dispose();
+stopAudio();
   }
 
-  void checkUserAnswer(BuildContext context, String submittedAnswerChoice){
+  void checkUserAnswer(BuildContext context, String submittedAnswerChoice) {
+    // print(submittedAnswerChoice);
 
-   // print(submittedAnswerChoice);
+    stopAudio();
+    print("submittedAnswerChoice---------$submittedAnswerChoice ---------Correct Ans:$_correctAnswer");
 
     if (submittedAnswerChoice == _correctAnswer){
-      print(submittedAnswerChoice);
+  //    print(submittedAnswerChoice);
 
+    AwesomeDialog(
 
+      animType: AnimType.SCALE,
+
+      context: context,
+      title: "Correct",
+      dialogType: DialogType.SUCCES,
+      dismissOnTouchOutside: false,
+      customHeader: Container(
+        decoration: BoxDecoration(color: Colors.transparent),
+        child: Icon(
+          Icons.check,
+          color: Colors.green,
+          size: 100,
+        ),
+      ),
+
+      body: Container(height: 300,color: Colors.lightBlue,),
+      btnOk: LayoutBuilder(
+        builder: (context, BoxConstraints constraint){
+          print(constraint.maxWidth);
+          return AnimatedButton(width: constraint.maxWidth - 10, child: Text("OK"), onPressed: (){
+            Navigator.pop(context);
+            Navigator.pop(context);
+
+          }, color: Colors.green,);
+        },
+          
+          
+   //   btnCancel: AnimatedButton(width: -60,child: Text("OK"), onPressed: (){}, color: Colors.green,)
+    ),
+
+    ).show();
 
     } else{
       print("Wrong answer$submittedAnswerChoice");
+
+      AwesomeDialog(
+
+        context: context,
+
+        title: "Wrong",
+        dialogType: DialogType.ERROR,
+        dismissOnTouchOutside: false,
+        customHeader: Container(
+          decoration: BoxDecoration(color: Colors.transparent),
+          child: Icon(
+            Icons.close,
+            color: Colors.red,
+            size: 100,
+          ),
+        ),
+
+        body: Container(height: 300,color: Colors.lightBlue,),
+        btnOk: LayoutBuilder(
+          builder: (context, BoxConstraints constraint){
+            print(constraint.maxWidth);
+            return AnimatedButton(width: constraint.maxWidth - 10, child: Text("OK"), onPressed: (){
+              Navigator.pop(context);
+              Navigator.pop(context);
+
+            }, color: Colors.green,);
+          },
+
+
+          //   btnCancel: AnimatedButton(width: -60,child: Text("OK"), onPressed: (){}, color: Colors.green,)
+        ),
+
+      ).show();
     }
-
-
-
   }
-
 
   @override
   Widget build(BuildContext context) {
 //    print("Wrong Answer: ${singer.subCategories[0].getWrongAnswer()}");
 //    print("Wrong Answer: ${singer.subCategories[0].getWrongAnswer()}");
 //    print("Wrong Answer: ${singer.subCategories[0].getWrongAnswer()}");
-  //  print("Correct Answer: ${singer.questionList[0].correctAnswer}");
+    //  print("Correct Answer: ${singer.questionList[0].correctAnswer}");
 
     return Scaffold(
       body: SingleChildScrollView(
@@ -176,8 +245,14 @@ class _AnswerSelectionPageState extends State<AnswerSelectionPage> {
                 decoration: BoxDecoration(
                     color: Colors.grey[200],
                     borderRadius: BorderRadius.circular(15)),
-
-                child: Center(child: Container(padding: EdgeInsets.all(10), child: Text("Listen to the audio clip and determine who the speaker is.", style: TextStyle(fontSize: 15, fontWeight:FontWeight.w700 ),))),
+                child: Center(
+                    child: Container(
+                        padding: EdgeInsets.all(10),
+                        child: Text(
+                          "Listen to the audio clip and determine who the speaker is.",
+                          style: TextStyle(
+                              fontSize: 15, fontWeight: FontWeight.w700),
+                        ))),
               ),
 
               Center(
@@ -185,7 +260,7 @@ class _AnswerSelectionPageState extends State<AnswerSelectionPage> {
                   child: Text(_multipleChoices[0]),
                   color: Colors.grey[100],
                   width: MediaQuery.of(context).size.width - 60,
-                  onPressed: ()  {
+                  onPressed: () {
                     checkUserAnswer(context, _multipleChoices[0]);
                   },
                 ),
@@ -199,7 +274,6 @@ class _AnswerSelectionPageState extends State<AnswerSelectionPage> {
                   color: Colors.grey[100],
                   width: MediaQuery.of(context).size.width - 60,
                   onPressed: () {
-
                     checkUserAnswer(context, _multipleChoices[1]);
                   },
                 ),
@@ -213,7 +287,6 @@ class _AnswerSelectionPageState extends State<AnswerSelectionPage> {
                   color: Colors.grey[100],
                   width: MediaQuery.of(context).size.width - 60,
                   onPressed: () {
-
                     checkUserAnswer(context, _multipleChoices[2]);
                   },
                 ),
@@ -235,7 +308,6 @@ class _AnswerSelectionPageState extends State<AnswerSelectionPage> {
           ),
         ),
       ),
-
     );
   }
 }
